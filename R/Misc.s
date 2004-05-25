98400be98f76c9f1561aba8008a36f6343eec229
harrelfe <harrelfe@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2004-05-25 12:45:26 +0000

    Added makeNames function to make it easy to convert _ to . by default with make.names, unlike R 1.9.

--- R/Misc.s
+++ R/Misc.s
@@ -1319,3 +1319,13 @@ if (length(x) == 0)
   return(character())
 paste("\"", x, "\"", sep = "")
 }
+
+makeNames <- function(names, unique=FALSE, allow=NULL) {
+ ## Runs make.names with exceptions in vector allow
+ ## By default, R 1.9 make.names is overridden to convert _ to . as
+ ## with S-Plus and previous versions of R.  Specify allow='_' otherwise.
+ if(!.R. & length(allow)) stop('does not apply for S-Plus')
+ n <- make.names(names, unique)
+ if(!length(allow)) n <- gsub('_', '.', n)
+ n
+}

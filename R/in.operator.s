c5c24ac0dfc304a40eeff7126ef24acf12aeb7e0
harrelfe <harrelfe@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2004-08-20 02:57:15 +0000

    Removed %in% function; caused survey package to fail by overriding builtin %in% which does not ever convert a factor to its numeric codes.  Thanks: andrewr@uidaho.edu

--- R/in.operator.s
+++ R/in.operator.s
@@ -1,15 +1,1 @@
-"%in%" <- function(a,b)				{
-
-if(is.factor(a) & is.numeric(b))	{
-   warning("a is factor, b is numeric.  Assuming b is coded factor values")
-   a <- oldUnclass(a)			}
-
-else if(is.numeric(a) && is.factor(b))	{
-   warning("a is numeric, b is factor.  Assuming a is coded factor values")
-   b <- oldUnclass(b)			}
-
-match(a, b, nomatch=0) > 0
-						}
-
-
 "%nin%" <- function(a, b) ! (a %in% b)

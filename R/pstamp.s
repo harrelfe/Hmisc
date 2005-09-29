dbda8ea695a9efd7bebef73840b1f32c389c9f15
dupontct <dupontct@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2005-09-29 22:31:40 +0000

    fixed logrithmic plot bug

--- R/pstamp.s
+++ R/pstamp.s
@@ -1,17 +1,28 @@
 pstamp <- if(.R.) function(txt, pwd=FALSE, time.=TRUE)
 {
-  stamp <- function(string, ...)
+  stamp <- function(string = Sys.time(), print = TRUE, plot = TRUE)
   {
     opar <- par(yaxt='s',xaxt='s',xpd=NA)
     on.exit(par(opar))
     plt <- par('plt')
     usr <- par('usr')
-    text(usr[2] + diff(usr[1:2])/diff(plt[1:2])*
-         (1-plt[2]) - .6*strwidth('m'),
-         usr[3] - diff(usr[3:4])/diff(plt[3:4])*plt[3] +
-         .6*strheight('m'),
-         string, adj=1)
-    invisible()
+
+    ## when a logrithmic scale is in use (i.e. par('xlog') is true),
+    ## then the x-limits would be 10^par('usr')[1:2].  Similarly for
+    ## the y axis
+    xcoord <- usr[2] + (usr[2] - usr[1])/(plt[2] - plt[1]) *
+      (1-plt[2]) - .6*strwidth('m')
+    ycoord <- usr[3] - diff(usr[3:4])/diff(plt[3:4])*(plt[3]) +
+      0.6*strheight('m')
+      
+    if(par('xlog'))
+      xcoord <- 10^(xcoord)
+    if(par('ylog'))
+      ycoord <- 10^(ycoord)
+
+    ## Print the text on the current plot
+    text(xcoord, ycoord, string, adj=1)
+    invisible(string)
   }
 
   date.txt <- if(time.) format(Sys.time())
@@ -20,14 +31,12 @@ pstamp <- if(.R.) function(txt, pwd=FALSE, time.=TRUE)
   if(pwd)
     date.txt <- paste(getwd(), date.txt)
 
-  old <- par(c('mfrow','cex'))
-  par(mfrow=c(1,1))
-  par(cex=.5)
+  oldpar <- par(mfrow=c(1,1), cex = 0.5)
+  on.exit(par(oldpar))
   if(!missing(txt))
     date.txt <- paste(txt,'   ',date.txt, sep='')
   
   stamp(string=date.txt,print=FALSE,plot=TRUE)
-  par(old)
   invisible()
 
 } else function(txt, pwd=FALSE, time.=under.unix)
@@ -43,17 +52,11 @@ pstamp <- if(.R.) function(txt, pwd=FALSE, time.=TRUE)
       stop('time.=T not supported')
   }
                  
-  if(pwd) {
-    if(!under.unix)
-      stop('pwd not supported except with Linux/UNIX')
-    
-    pwd <- unix('pwd')
-    date.txt <- paste(pwd, date.txt)
-  }
+  if(pwd)
+    date.txt <- paste(getwd(), date.txt)
   
-  old <- par(c('mfrow','cex'))
-  par(mfrow=c(1,1))
-  par(cex=.5)
+  oldpar <- par(mfrow = c(1,1), cex = 0.5)
+  on.exit(par(oldpar))
   if(!missing(txt))
     date.txt <- paste(txt,'   ',date.txt, sep='')
   

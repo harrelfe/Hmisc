e0e9f67f3eb47eba02d5a550750f21eaae3c96fb
harrelfe <harrelfe@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2006-11-27 15:27:38 +0000

    added chiSquare.* functions

--- R/rcorr.s
+++ R/rcorr.s
@@ -208,8 +208,7 @@ plot.spearman2.formula <- function(x,
 }
 
 chiSquare <- function(x, g=3, data, subset, na.action,
-                              minlev=0, exclude.imputed=TRUE, ...)
-{
+                      minlev=0, exclude.imputed=TRUE, ...) {
   call <- match.call()
   nact <- NULL
   y <- match.call(expand=FALSE)
@@ -218,7 +217,7 @@ chiSquare <- function(x, g=3, data, subset, na.action,
   if(missing(na.action)) y$na.action <- na.retain
   y[[1]] <- as.name("model.frame")
   x <- eval(y, sys.parent())
-  nam <- names(x); yname <- names(x)[1]
+  nam <- names(x); yname <- nam[1]
   y <- x[[1]]
   if(minlev) y <- combine.levels(y, minlev=minlev)
   x <- x[-1]
@@ -230,23 +229,23 @@ chiSquare <- function(x, g=3, data, subset, na.action,
     j <- !(is.na(w) | is.na(y))
     if(exclude.imputed) j <- j & !(is.imputed(w) | is.imputed(y))
     yy <- y[j]; w <- w[j]
-    if(is.category(w) && minlev) w <- combine.levels(w, minlev=minlev)
+    if((is.character(w) || is.category(w)) && minlev)
+      w <- combine.levels(w, minlev=minlev)
   
     N[i] <- length(w)
     if(is.numeric(w) && length(unique(w)) > g) w <- cut2(w, g=g)
-    ct <- chisq.test(w, yy)
+    ct <- chisq.test(w, yy, ...)
     chisq[i] <- ct$statistic
     df[i]    <- ct$parameter
     pval[i]  <- ct$p.value
   }
-  w <- cbind(chisquare=chisq, df=df, "chisquare-df"=chisq-df, p=pval, n=N)
+  w <- cbind(chisquare=chisq, df=df, "chisquare-df"=chisq-df, P=pval, n=N)
   dimnames(w)[[1]] <- names(x)
   structure(w, class='chiSquare', yname=yname)
 }
 
 
-print.chiSquare <- function(x, ...)
-{
+print.chiSquare <- function(x, ...) {
   cat('\nPearson Chi-square Tests    Response variable:',attr(x,'yname'),'\n\n')
   dig <- c(2,0,2,4,0)
   for(i in 1:5)
@@ -260,16 +259,14 @@ print.chiSquare <- function(x, ...)
 
 plot.chiSquare <- function(x,
                            what=c('chisquare-df','chisquare','P'),
-                           sort.=TRUE, main, xlab, ...)
-{
+                           sort.=TRUE, main, xlab, ...) {
   what <- match.arg(what)
-  if(missing(xlab)) xlab <- switch(what,
-                                   'chisquare-df'=
-                                     if(.R.)expression(chi^2 - d.f.)
-                                     else 'Chi-square - d.f.',
-                                   'chisquare'=if(.R.)expression(chi^2)
-                                          else 'Chi-square',
-                                   'P'='P-value')
+  if(missing(xlab))
+    xlab <- switch(what,
+                   'chisquare-df'=
+                   if(.R.)expression(chi^2 - d.f.) else 'Chi-square - d.f.',
+                   chisquare=if(.R.)expression(chi^2) else 'Chi-square',
+                   P        ='P-value')
   if(missing(main))
     main <- if(.R.) parse(text=paste('paste(Pearson,~chi^2,~~~~Response:',
                                      attr(x,'yname'),')',sep=''))
@@ -278,7 +275,7 @@ plot.chiSquare <- function(x,
   
   if(.SV4.) x <- matrix(oldUnclass(x), nrow=nrow(x),
                         dimnames=dimnames(x))
-  aux <- paste(x[,'n'],x[,'df'])
+  aux <- paste(format(x[,'n']),format(x[,'df']))
   stat <- x[,what]
   if(sort.) {
     i <- order(stat)

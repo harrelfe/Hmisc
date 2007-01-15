2b883a8076f976091bdf0a8e17cf942fd5540b7f
harrelfe <harrelfe@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2007-01-15 23:24:04 +0000

    had forgotten to update defaults for nk, tlinear

--- R/aregImpute.s
+++ R/aregImpute.s
@@ -1,6 +1,6 @@
 # $Id$
 aregImpute <- function(formula, data, subset, n.impute=5,
-                       group=NULL, nk=0, tlinear=FALSE,
+                       group=NULL, nk=3, tlinear=TRUE,
                        type=c('pmm','regression'),
                        match=c('weighted','closest'), fweighted=0.2,
                        burnin=3, x=FALSE,

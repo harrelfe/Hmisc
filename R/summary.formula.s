009be699fc260e21e821dd8f1c79511d1b7f9ec6
scottt <scottt@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2007-02-14 14:37:59 +0000

    Modified the ordTest argument once again.  Actually, need to specify 'character.only = TRUE' in 'requirePackage('Design', ...)' for it to work correctly.

--- R/summary.formula.s
+++ R/summary.formula.s
@@ -34,10 +34,8 @@ summary.formula <-
                   plotmathstat='chi[df]^2')
            },
            ordTest=function(group, x) {
-#             requirePackage('Design')
-# require and requirePackage will only accept a character string
-# if character.only = TRUE, so just give the Design package name instead
-             requirePackage(Design)
+             requirePackage('Design', character.only = TRUE)
+             # must specify 'character.only = TRUE' for requirePackage to work
              f <- lrm(x ~ group)$stats
              list(P=f['P'], stat=f['Model L.R.'], df=f['d.f.'],
                   testname='Proportional odds likelihood ratio',

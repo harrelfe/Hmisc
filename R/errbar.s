b65b8ce212df7e28f8da6adaa6f93f202d981f3c
harrelfe <harrelfe@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2007-02-27 23:25:04 +0000

    ... was not passed to enough graphics functions; add=TRUE did not result in points being plotted; fixed

--- R/errbar.s
+++ R/errbar.s
@@ -5,14 +5,14 @@
 ## Modified 26Aug94 FEH - added explicit lwd parameter for segments()
 ## FEH 2Jul02 added horizontal charts with differences on 2nd axis
 
-errbar <- function(x, y, yplus, yminus, cap=.015,
-                   xlab=as.character(substitute(x)),
-                   ylab=if(is.factor(x) || is.character(x))
-                          ''
-                        else
-                          as.character(substitute(y)),
-                   add=FALSE, lty=1, ylim, lwd=1,
-                   Type=rep(1,length(y)), ... )
+errbar <-
+  function(x, y, yplus, yminus, cap=.015,
+           xlab=as.character(substitute(x)),
+           ylab=if(is.factor(x) || is.character(x)) ''
+           else
+           as.character(substitute(y)),
+           add=FALSE, lty=1, ylim, lwd=1,
+           Type=rep(1,length(y)), ... )
 {
   if(missing(ylim))
     ylim <- range(y[Type==1], yplus[Type==1], yminus[Type==1],
@@ -25,13 +25,13 @@ errbar <- function(x, y, yplus, yminus, cap=.015,
     t2 <- Type==2
     n1 <- sum(t1)
     n2 <- sum(t2)
-
+    
     omai <- par('mai')
     mai <- omai
     mai[2] <- max(strwidth(x, 'inches')) + .25 * .R.
     par(mai=mai)
     on.exit(par(mai=omai))
-    plot(0,0,xlab=ylab,ylab='',xlim=ylim,ylim=c(1,n+1),axes=FALSE)
+    plot(0,0,xlab=ylab,ylab='',xlim=ylim,ylim=c(1,n+1),axes=FALSE,...)
     axis(1)
     w <-
       if(any(t2))
@@ -39,19 +39,18 @@ errbar <- function(x, y, yplus, yminus, cap=.015,
       else
         numeric(0)
     
-    axis(2, at=c(1:n1,w), labels=c(x[t1],x[t2]),
-         las=1,adj=1)
-    points(y[t1], 1:n1, pch=16)
-    segments(yplus[t1], 1:n1, yminus[t1], 1:n1)
+    axis(2, at=c(1:n1,w), labels=c(x[t1],x[t2]), las=1,adj=1)
+    points(y[t1], 1:n1, pch=16, ...)
+    segments(yplus[t1], 1:n1, yminus[t1], 1:n1, ...)
 
     if(any(Type==2)) {
-      abline(h=n1+1, lty=2)
+      abline(h=n1+1, lty=2, ...)
       offset <- mean(y[t1]) - mean(y[t2])
       if(min(yminus[t2]) < 0 & max(yplus[t2]) > 0)
-        lines(c(0,0)+offset, c(n1+1,par('usr')[4]), lty=2)
+        lines(c(0,0)+offset, c(n1+1,par('usr')[4]), lty=2, ...)
       
-      points(y[t2] + offset, w, pch=16)
-      segments(yminus[t2]+offset, w, yplus[t2]+offset, w)
+      points(y[t2] + offset, w, pch=16, ...)
+      segments(yminus[t2]+offset, w, yplus[t2]+offset, w, ...)
       at <- pretty(range(y[t2],yplus[t2],yminus[t2]))
       axis(3, at=at+offset, label=format(round(at,6)))
     }
@@ -59,13 +58,14 @@ errbar <- function(x, y, yplus, yminus, cap=.015,
     return(invisible())
   }
   
-  if(!add)
+  if(add) points(x, y, ...)
+  else
     plot(x, y, ylim=ylim, xlab=xlab, ylab=ylab, ...)
   
   xcoord <- par()$usr[1:2]
-  segments(x, yminus, x, yplus , lty=lty, lwd=lwd)
+  segments(x, yminus, x, yplus , lty=lty, lwd=lwd, ...)
   smidge <- cap * ( xcoord[2] - xcoord[1] ) / 2
-  segments( x - smidge, yminus, x + smidge, yminus, lwd=lwd)
-  segments( x - smidge, yplus, x + smidge, yplus, lwd=lwd)
+  segments( x - smidge, yminus, x + smidge, yminus, lwd=lwd, ...)
+  segments( x - smidge, yplus, x + smidge, yplus, lwd=lwd, ...)
   invisible()
 }

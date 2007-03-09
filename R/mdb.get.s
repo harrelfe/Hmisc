910551b4bbf41eb4f37a6defe06ed5c8ebccbea8
harrelfe <harrelfe@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2007-03-09 22:32:58 +0000

    New function to import a Microsoft Access database

---
+++ R/mdb.get.s
@@ -0,0 +1,31 @@
+mdb.get <- function(file, tables=NULL, lowernames=FALSE, allow=NULL, ...)
+{
+  rettab <- length(tables) && is.logical(tables)
+  if(rettab) tables <- NULL
+  if(!length(tables))
+    tables <- system(paste('mdb-tables -1', file), intern=TRUE)
+  if(rettab) return(tables)
+
+  f <- tempfile()
+  D <- vector('list', length(tables))
+  names(D) <- tables
+
+  for(tab in tables) {
+    s <- system(paste('mdb-schema -T', dQuote(tab), file), intern=TRUE)
+    start <- grep('^ \\($', s) + 1
+    end   <- grep('^\\);$', s) - 1
+    s <- s[start:end]
+    s <- strsplit(s, '\t')
+    vnames <- sapply(s, function(x)x[2])
+    vnames <- makeNames(vnames, unique=TRUE, allow=allow)
+    if(lowernames) vnames <- casefold(vnames)
+    types  <- sapply(s, function(x)x[length(x)])
+    datetime <- vnames[grep('DateTime', s)]
+    system(paste('mdb-export', file, dQuote(tab), '>', f))
+    d <- csv.get(f, datetimevars=datetime,
+                 lowernames=lowernames, allow=allow, ...)
+    if(length(tables) == 1) return(d)
+    else D[[tab]] <- d
+  }
+  D
+}

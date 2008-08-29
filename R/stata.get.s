5ce95976e4785587c11e79ff9cdd1ec59adadfa1
dupontct <dupontct@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2008-08-29 21:02:59 +0000

    renamed function stata.get2 back to stata.get.

--- R/stata.get.s
+++ R/stata.get.s
@@ -1,5 +1,5 @@
 if (.R.) {
-  stata.get2 <- function(file, lowernames=FALSE,
+  stata.get <- function(file, lowernames=FALSE,
                         convert.dates=TRUE, convert.factors=TRUE,
                         missing.type=FALSE, convert.underscore=TRUE,
                         warn.missing.labels=TRUE, force.single=TRUE,

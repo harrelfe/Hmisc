0c9811a6071a955ae2e2a7cd7105f49c85312ec6
dupontct <dupontct@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2010-07-13 21:31:12 +0000

    merged cbind default behavoir fix for format.df

--- R/latex.s
+++ R/latex.s
@@ -349,7 +349,15 @@ format.df <- function(x,
             col.just[j]
           else 'l'
         
-        cxk <- cleanLatex(xk)
+        if(inherits(xk, "Date")) {
+          cxk <- cleanLatex(format(xk, format=format.Date))
+        } else if(inherits(xk, "POSIXt")) {
+          cxk <- cleanLatex(format(xk, format=format.POSIXt))
+        } else if(inherits(xk, "factor")) {
+          cxk <- cleanLatex(as.character(xk))
+        } else {
+          cxk <- cleanLatex(xk)
+        }
       }
       
       cx <- cbind(cx, cxk)

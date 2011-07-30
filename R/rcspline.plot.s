47c5c0fa15d0403976b5b578d239ae1fa70afd38
harrelfe <harrelfe@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2011-07-30 22:06:09 +0000

    Typo, lrm should have been rms

--- R/rcspline.plot.s
+++ R/rcspline.plot.s
@@ -78,7 +78,7 @@ rcspline.plot <- function(x, y, model=c("logistic","cox","ols"), xrange,
 
   df1 <- nk-2
   if(model=="logistic") {
-    requirePackage('lrm')
+    requirePackage('rms')
     
     b <- lrm.fit(cbind(x,xx,adj),y)
     ##b <- glim(cbind(x,xx,adj),y,rep(1,n),error="binomial",

8bb8079dd9b9f0ca40a9aa28289aa0d8eec72bb0
harrelfe <harrelfe@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2013-06-22 20:19:09 +0000

    allowed kint to be NULL in matxv and b to be a matrix, such as boostrap coefficient vector repititions

--- R/matxv.s
+++ R/matxv.s
@@ -9,9 +9,20 @@
 ##            14 Nov91 - changed to nrow=1 if length(b)>1, ncol=1 otherwise
 ##            25 Mar93 - changed to use %*%
 ##            13 Sep93 - added kint parameter
+##            22 Jun13 - allowed null kint, matrix b (e.g. bootstrap coefs)
 
-matxv <- function(a, b, kint=1)
+matxv <- function(a, b, kint=1, bmat=FALSE)
 {
+  if(bmat) {
+    if(!is.matrix(a)) stop('a must be a matrix when b is a matrix')
+    ca <- ncol(a); cb <- ncol(b)
+    if(cb < ca) stop('number of columns in b must be >= number in a')
+    ab <- t(a %*% t(b[, (cb - ca + 1) : cb, drop=FALSE]))
+    if(ca == cb) return(ab)
+    bkint <- if(length(kint)) b[, kint] else 0.
+    return(bkint + ab)
+  }
+
   if(!is.matrix(a))
     a <- if(length(b) == 1L) matrix(a, ncol=1L) else matrix(a, nrow=1L)
 

a81768a9e040ac174587867a9e63ff3b1d91dc51
harrelfe <harrelfe@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2013-07-03 19:36:18 +0000

    enhanced matxv to sense "intercepts" attribute of b.  Used by rms.

--- R/matxv.s
+++ R/matxv.s
@@ -10,16 +10,35 @@
 ##            25 Mar93 - changed to use %*%
 ##            13 Sep93 - added kint parameter
 ##            22 Jun13 - allowed null kint, matrix b (e.g. bootstrap coefs)
+##             3 Jul13 - sense intercepts attribute in b which signals
+##                       which subset of intercepts were retained in fit
 
 matxv <- function(a, b, kint=1, bmat=FALSE)
 {
+  bi    <- attr(b, 'intercepts')
+  lbi   <- length(bi)
+  lkint <- length(kint)
+  if(lkint > 1L) stop('kint must have length 0 or 1')
+  
   if(bmat) {
     if(!is.matrix(a)) stop('a must be a matrix when b is a matrix')
     ca <- ncol(a); cb <- ncol(b)
     if(cb < ca) stop('number of columns in b must be >= number in a')
     if(cb == ca) return(a %*% t(b))
-    xx <- matrix(0, nrow=nrow(a), ncol=cb - ca)
-    if(length(kint)) xx[, kint] <- 1.
+    excess <- cb - ca
+    xx <- matrix(0, nrow=nrow(a), ncol=excess)
+    if(lbi && lkint) {
+      if(lbi != excess)
+        stop('b intercepts attribute has different length from number of excess elements in b')
+      if(!isTRUE(all.equal(sort(bi), sort(kint))))
+        stop('b intercepts attribute do not match kint')
+      xx[] <- 1.
+    }
+    else if(lkint) {
+      if(kint > excess)
+        stop('kint > number of excess elements in b')
+      xx[,kint] <- 1.
+    }
     return(cbind(xx, a) %*% t(b))
   }
 
@@ -32,10 +51,23 @@ matxv <- function(a, b, kint=1, bmat=FALSE)
     stop(paste("columns in a (", nc, ") must be <= length of b (",
                length(b), ")", sep=""))
 
-  if(nc == lb)
-    drop(a %*% b)
-  else {
-    bkint <- if(length(kint)) b[kint] else 0.
-    drop(bkint + (a %*% b[(lb - nc + 1L) : lb]))
+  if(nc == lb) return(drop(a %*% b))
+
+  excess <- lb - nc
+  if(lbi && lkint) {
+    if(lbi != excess)
+      stop('b intercepts attribute has different length from number of excess elements in b')
+    if(!isTRUE(all.equal(sort(bi), sort(kint))))
+      stop('b intercepts attribute do not match kint')
+    bkint <- b[1]
   }
+  else if(lkint) {
+    if(kint > excess)
+      stop('kint > number excess elements in b')
+    
+    bkint <- b[kint]
+  }
+  else
+    bkint <- 0.
+  drop(bkint + (a %*% b[(lb - nc + 1L) : lb]))
 }

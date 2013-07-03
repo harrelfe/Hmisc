a6aa3737a551cfe896e48cf737e9f3d72a31b363
dupontct <dupontct@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2013-07-03 15:33:28 +0000

    cleaned up extranious refrences to dropUnusedLevels functions

--- R/AFirst.lib.s
+++ R/AFirst.lib.s
@@ -16,8 +16,6 @@ if(!exists('existsFunction')) {
   if(!length(verbose) || verbose)
     packageStartupMessage("Hmisc library by Frank E Harrell Jr\n\n",
         "Type library(help='Hmisc'), ?Overview, or ?Hmisc.Overview')\n",
-        "to see overall documentation.\n\n",
-        "NOTE:Hmisc no longer redefines [.factor to drop unused levels when\n",
-        "subsetting.  To get the old behavior of Hmisc type dropUnusedLevels().\n")
+        "to see overall documentation.\n")
   invisible()
 }

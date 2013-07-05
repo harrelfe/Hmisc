018d18e051adc377452caf06215b3ebf0be5b7f2
harrelfe <harrelfe@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2013-07-05 11:50:35 +0000

    allow non-integer intercept positions; rounded

--- R/matxv.s
+++ R/matxv.s
@@ -30,8 +30,8 @@ matxv <- function(a, b, kint=1, bmat=FALSE)
     if(lbi && lkint) {
       if(lbi != excess)
         stop('b intercepts attribute has different length from number of excess elements in b')
-      bi   <- as.integer(bi)
-      kint <- as.integer(kint)
+      bi   <- round(bi)
+      kint <- round(kint)
       if(!isTRUE(all.equal(sort(bi), sort(kint))))
         stop('b intercepts attribute do not match kint')
       xx[] <- 1.
@@ -59,8 +59,8 @@ matxv <- function(a, b, kint=1, bmat=FALSE)
   if(lbi && lkint) {
     if(lbi != excess)
       stop('b intercepts attribute has different length from number of excess elements in b')
-    bi   <- as.integer(bi)
-    kint <- as.integer(kint)
+    bi   <- round(bi)
+    kint <- round(kint)
     if(!isTRUE(all.equal(sort(bi), sort(kint))))
       stop('b intercepts attribute do not match kint')
     bkint <- b[1]

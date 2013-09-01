93b693dca9fd9564c8a52c1f768a2804dbaf8ce0
harrelfe <harrelfe@d4f8e6dd-0978-41d7-90e5-37873652adc8>
2013-09-01 12:46:18 +0000

    new test for latex with ctable or booktabs and landscape

---
+++ tests/latex.s
@@ -0,0 +1,8 @@
+require(Hmisc)
+x <- cbind(x1=1:5, x2=2:6)
+f <- '/tmp/z.tex'
+cat('\\documentclass{article}\n\\usepackage{lscape,ctable,booktabs}\n\\begin{document}\n', file=f)
+w <- latex(x, booktabs=TRUE, landscape=TRUE, file=f, append=TRUE)
+w <- latex(x, ctable=TRUE, landscape=TRUE, file=f, append=TRUE)
+cat('\\end{document}\n', file=f, append=TRUE)
+

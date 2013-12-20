d0e72a48ce5dd7deada1766748fa084ee4b4265e
harrelfe <f.harrell@vanderbilt.edu>
2013-12-20 08:04:26 -0600

    ynbind: new function; summaryP: require groups in plot method to be character string

--- R/summaryP.s
+++ R/summaryP.s
@@ -4,56 +4,7 @@ summaryP <- function(formula, data=NULL,
                      asna=c('unknown', 'unspecified')) {
   
   formula <- Formula(formula)
-  environment(formula) <- new.env(parent = environment(formula))
-  Asna <- asna
-  Sort <- sort
-  yn <- function(..., label=deparse(substitute(...)), asna=Asna, sort=Sort) {
-    w <- list(...)
-    k <- length(w)
-    if(! k) stop('no variables to process')
-    nam <- as.character(sys.call())[-1]
-    nam <- nam[1 : k]
-    lab <- nam
-    W <- matrix(NA, nrow=length(w[[1]]), ncol=k, dimnames=list(NULL, nam))
-    for(j in 1:k) {
-      x <- w[[j]]
-      na <- is.na(x)
-      la <- label(x)
-      if(la != '') lab[j] <- la
-      if(is.numeric(x) && all(x %in% 0 : 1)) x <- x == 1
-      if(! is.logical(x)) {
-        x <- tolower(as.character(x))
-        if(length(asna)) {
-          i <- x %in% asna
-          if(any(i)) na[i] <- TRUE
-        }
-        x <- x %in% c('y', 'yes', 'present')
-        if(any(na)) x[na] <- NA
-      }
-      W[, j] <- x
-    }
-    ## Sort columns in ascending order of overall proportion
-    prop <- apply(W, 2, mean, na.rm=TRUE)
-    if(sort) {
-      i <- order(prop)
-      W <- W[, i, drop=FALSE]
-      lab <- lab[i]
-    }
-    structure(W, label=label, labels=lab, class=c('yn', 'matrix'))
-  }
-  assign(envir = environment(formula), 'yn', yn)
-  '[.yn' <- function(x, ...) {
-    at <- attributes(x)[c('label', 'labels')]
-    x <- NextMethod('[')
-    attributes(x) <- c(attributes(x), at)
-    class(x) <- 'yn'
-    x
-  }
-  
-  lhs <- terms(formula, lhs=1, specials='yn')
-  rhs <- terms(formula, rhs=1)
-  cl <- attr(lhs, 'specials')
-  
+
   Y <- if(length(subset))
     model.frame(formula, data=data, subset=subset, na.action=na.action)
   else
@@ -69,12 +20,12 @@ summaryP <- function(formula, data=NULL,
   Z <- NULL
   n <- nrow(X)
 
-  if(Sort) {
+  if(sort) {
     ## Compute marginal frequencies of all regular variables so can sort
     mfreq <- list()
     for(ny in namY) {
       y <- Y[[ny]]
-      if(!inherits(y, 'yn')) {
+      if(!inherits(y, 'ynbind')) {
         if(length(asna) && (is.factor(y) || is.character(y)))
           y[y %in% asna] <- NA
         freq <- table(y)
@@ -84,6 +35,7 @@ summaryP <- function(formula, data=NULL,
       }
     }
   }
+  nobs <- 0
   for(i in 1 : nrow(ux)) {
     j <- rep(TRUE, n)
     if(nX > 0) for(k in 1 : nX) j <- j & (X[[k]] == ux[i, k])
@@ -93,16 +45,18 @@ summaryP <- function(formula, data=NULL,
       y <- Y[[k]]
       y <- if(is.matrix(y)) y[j,, drop=FALSE] else y[j]
 #      y <- (Y[[k]])[j,, drop=FALSE]
-      if(inherits(y, 'yn')) {
+      if(inherits(y, 'ynbind')) {
         overlab <- attr(y, 'label')
         labs <- attr(y, 'labels')
         z <- NULL
         for(iy in 1 : ncol(y)) {
           tab <- table(y[, iy])
+          no <- as.numeric(sum(tab))
+          nobs <- max(nobs, no)
           z <- rbind(z,
                      data.frame(var=overlab, val=labs[iy],
                                 freq=as.numeric(tab['TRUE']),
-                                denom=as.numeric(sum(tab))))
+                                denom=no))
         }
       }
       else {  # regular single column
@@ -114,17 +68,19 @@ summaryP <- function(formula, data=NULL,
         if(la == '') la <- ny
         lev <- names(tab)
         mf <- mfreq[[ny]]
+        no <- as.numeric(sum(tab))
+        nobs <- max(nobs, no)
         if(exclude1 && length(mf) == 2) {
           lowest <- names(which.min(mf))
           z <- data.frame(var=la, val=lowest,
                           freq=as.numeric(tab[lowest]),
-                          denom=as.numeric(sum(tab)))
+                          denom=no)
         }
         else {
-          if(Sort) lev <- reorder(lev, (mfreq[[ny]])[lev])
+          if(sort) lev <- reorder(lev, (mfreq[[ny]])[lev])
           z <- data.frame(var=la, val=lev,
                           freq=as.numeric(tab),
-                          denom=as.numeric(sum(tab)))
+                          denom=no)
         }
       }
       ## Add current X subset settings
@@ -132,13 +88,14 @@ summaryP <- function(formula, data=NULL,
       Z <- rbind(Z, z)
     }
   }
-  structure(Z, class=c('summaryP', 'data.frame'), formula=formula, nX=nX, nY=nY)
+  structure(Z, class=c('summaryP', 'data.frame'), formula=formula,
+            nX=nX, nY=nY, nobs=nobs)
 }
 
 plot.summaryP <-
   function(x, formula=NULL, groups=NULL, xlim=c(0, 1), col=1:2, pch=1:2,
            cex.values=0.5, xwidth=.125, ydelta=0.04,
-           key=list(columns=length(levels(groups)),
+           key=list(columns=length(groupslevels),
              x=.75, y=-.04, cex=.9, col=col, corner=c(0,1)), outerlabels=TRUE, ...)
 {
   if(outerlabels) require(latticeExtra)
@@ -147,10 +104,9 @@ plot.summaryP <-
   Form <- at$formula
   nX   <- at$nX
   nY   <- at$nY
-  groupsname <- as.character(substitute(groups))
-  if(length(groupsname)) groups <- x[[groupsname]]
-  
-  condvar <- setdiff(names(X), c('val', 'var', 'freq', 'denom', groupsname))
+
+  groupslevels <- if(length(groups)) levels(x[[groups]])
+  condvar <- setdiff(names(X), c('val', 'var', 'freq', 'denom', groups))
   form <- if(length(formula)) formula
   else {
     form <- paste('val ~ freq | var')
@@ -191,16 +147,16 @@ plot.summaryP <-
       }
     }
   }
-  if(length(groupsname))
+  if(length(groups))
     trellis.par.set(superpose.symbol = list(col = col, pch=pch))
   else
     trellis.par.set(dot.symbol = list(col = col[1], pch=pch[1]))
-  d <- if(length(groupsname))
-    sprintf("dotplot(form, groups=%s, data=X, scales=list(y='free', rot=0), panel=pan, xlim=xlim, auto.key=key, xlab='Proportion', col=col)", groupsname)
+  d <- if(length(groups))
+    sprintf("dotplot(form, groups=%s, data=X, scales=list(y='free', rot=0), panel=pan, xlim=xlim, auto.key=key, xlab='Proportion', col=col)", groups)
   else sprintf("dotplot(form, data=X, scales=list(y='free', rot=0), panel=pan, xlim=xlim, auto.key=key, xlab='Proportion', col=col[1])")
   d <- eval(parse(text = d))
 
-  if(outerlabels && (nX - length(groupsname) + 1) == 2)
+  if(outerlabels && (nX - length(groups) + 1) == 2)
     d <- useOuterStrips(d)
   d
 }
---
+++ R/ynbind.s
@@ -0,0 +1,45 @@
+ynbind <- function(..., label=deparse(substitute(...)), asna=c('unknown', 'unspecified'), sort=TRUE) {
+w <- list(...)
+k <- length(w)
+if(! k) stop('no variables to process')
+nam <- as.character(sys.call())[-1]
+nam <- nam[1 : k]
+lab <- nam
+W <- matrix(NA, nrow=length(w[[1]]), ncol=k, dimnames=list(NULL, nam))
+for(j in 1 : k) {
+  x <- w[[j]]
+  na <- is.na(x)
+  la <- label(x)
+  if(la != '') lab[j] <- la
+  if(is.numeric(x) && all(x %in% 0 : 1)) x <- x == 1
+  if(! is.logical(x)) {
+    x <- tolower(as.character(x))
+    if(length(asna)) {
+      i <- x %in% asna
+      if(any(i)) na[i] <- TRUE
+    }
+    x <- x %in% c('y', 'yes', 'present')
+    if(any(na)) x[na] <- NA
+  }
+  W[, j] <- x
+}
+## Sort columns in ascending order of overall proportion
+prop <- apply(W, 2, mean, na.rm=TRUE)
+if(sort) {
+  i <- order(prop)
+  W <- W[, i, drop=FALSE]
+  lab <- lab[i]
+}
+structure(W, label=label, labels=lab, class=c('ynbind', 'matrix'))
+}
+
+'[.ynbind' <- function(x, rows=1:d[1], cols=1:d[2], ...) {
+  d <- dim(x)
+  at <- attributes(x)[c('label', 'labels')]
+  x <- NextMethod('[')
+  at$labels <- at$labels[cols]
+  attributes(x) <- c(attributes(x), at)
+  class(x) <- 'ynbind'
+  x
+  }
+ 

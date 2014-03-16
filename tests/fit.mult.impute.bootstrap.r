e7a9d86c6a4c805715ee2858629251c0b76a2c3c
harrelfe <f.harrell@vanderbilt.edu>
2014-03-16 07:50:05 -0500

    improved test

--- tests/fit.mult.impute.bootstrap.r
+++ tests/fit.mult.impute.bootstrap.r
@@ -5,13 +5,14 @@ n <- 101
 y  <- runif(n)
 y[1:2] <- NA
 x1 <- sample(c('a','b'), n, TRUE)
-x2 <- runif(n)
+x2 <- runif(n) + .15 * y
 d <- data.frame(y, x1, x2)
 a <- aregImpute(~ y + x1 + x2, burnin=10, n.impute=100, data=d)
 f <- fit.mult.impute(y ~ x1 + x2, ols, a, data=d)
 
 B <- 20 # actually use B=1000
 ranks <- matrix(NA, nrow=B, ncol=2)
+## Put - in front of plot in next line to have rank 1 = best
 rankvars <- function(fit) rank(plot(anova(fit), sort='none', pl=FALSE))
 Rank <- rankvars(f)
 for(i in 1:B) {
@@ -24,5 +25,7 @@ for(k in 1 : 2) {
   print(table(ranks[, k]))
   cat('\n')
 }
+
+lim <- t(apply(ranks, 2, quantile, probs=c(.025,.975)))
 
 

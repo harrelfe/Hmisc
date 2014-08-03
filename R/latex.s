37411f839741f3ec78c7bf4da004e49fa8e5a235
Frank Harrell <f.harrell@vanderbilt.edu>
2014-08-03 11:20:38 -0500

    format.df: fixed new bug in handling special characters

--- R/latex.s
+++ R/latex.s
@@ -78,16 +78,17 @@ format.df <- function(x,
     
     if(!any(k)) return(string)
 
-    inn <- c('< =', '> =', '<=', '>=', '<', '>', '%', '&')
+    inn <- c('< =', '> =', '<=', '>=', '<', '>',
+             '\\\\%', '%', 
+             '\\\\&', '&')
     out <- c('<=',
              '>=',
              paste('$', sl, sl, 'leq$', sep=''),
              paste('$', sl, sl, 'geq$', sep=''),
              paste(sl, sl, 'textless', sep=''),
              paste(sl, sl, 'textgreater', sep=''),
-             paste(sl, sl, '%', sep=''),
-             paste(sl, sl, '&', sep=''))
-
+             '%', paste(sl, sl, '%', sep=''),
+             '&', paste(sl, sl, '&', sep=''))
     for(i in 1 : length(inn))
       string[k] <- gsub(inn[i], out[i], string[k])
     string

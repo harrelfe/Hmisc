d7886d71d24a64913b62d3686524a041d26385a5
Frank Harrell <f.harrell@vanderbilt.edu>
2016-06-18 21:50:36 -0500

    ggfreqScatter: final tweaks

--- R/ggfreqScatter.r
+++ R/ggfreqScatter.r
@@ -19,8 +19,12 @@ ggfreqScatter <- function(x, y, bins=50, g=10,
     if(is.numeric(x)) k$x <- as.numeric(as.character(k$x))
     if(is.numeric(y)) k$y <- as.numeric(as.character(k$y))
     k$fg <- cut2(k$Freq, g=g)
-    ggplot(k, aes(x=x, y=y, alpha=fg)) + geom_point() +
-      guides(alpha=guide_legend(title='Frequency')) +
+    br   <- 1 : length(levels(k$fg))
+    labs <- levels(k$fg)
+    ggplot(k, aes(x=x, y=y, alpha=fg, color=as.integer(fg))) + geom_point() +
+      scale_color_gradientn(colors=rainbow(10), breaks=br, labels=labs) +
+      guides(alpha=FALSE, 
+             color=guide_legend(title='Frequency')) +
       xlab(xlab) + ylab(ylab)
 }
 

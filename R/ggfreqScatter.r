152ae9329c58bfbf59e6241ca0a1ca4d16317af2
Frank Harrell <f.harrell@vanderbilt.edu>
2016-06-19 11:42:38 -0500

    ggfreqScatter: added nsize, fcolors arguments

--- R/ggfreqScatter.r
+++ R/ggfreqScatter.r
@@ -7,43 +7,55 @@ ggfreqScatter <- function(x, y, bins=50, g=10,
                           yminor  = NULL,
                           xlab=as.character(substitute(x)),
                           ylab=as.character(substitute(y)),
-                          fcolors=rainbow(10), ...) {
+                          fcolors=rainbow(10), nsize=FALSE, ...) {
 
   xlab <- if(! missing(xlab)) xlab else if(label(x) != '') label(x, plot=TRUE) else xlab
   ylab <- if(! missing(ylab)) ylab else if(label(y) != '') label(y, plot=TRUE) else ylab
 
-  xbreaks <- xbreaks; ybreaks <- ybreaks
+  nx <- is.numeric(x); ny <- is.numeric(y)
+  xbreaks <- if(nx) xbreaks; ybreaks <- if(ny) ybreaks
   bins <- rep(bins, length=2)
   
   i <-  ! (is.na(x) | is.na(y))
   x <- xtrans(x[i]); y <- ytrans(y[i])
   
-  if(is.numeric(x)) {
+  if(nx) {
     rx <- range(x)
     sx <- diff(rx) / bins[1]
     x  <- rx[1] + sx * round((x - rx[1]) / sx)
   }
-  if(is.numeric(y)) {
+  if(ny) {
     ry <- range(y)
     sy <- diff(ry) / bins[2]
     y  <- ry[1] + sy * round((y - ry[1]) / sy)
   }
   
   k <- subset(as.data.frame(table(x, y)), Freq > 0)
-  if(is.numeric(x)) k$x <- as.numeric(as.character(k$x))
-  if(is.numeric(y)) k$y <- as.numeric(as.character(k$y))
+  if(nx) k$x <- as.numeric(as.character(k$x))
+  if(ny) k$y <- as.numeric(as.character(k$y))
   k$fg <- cut2(k$Freq, g=g)
-  br   <- 1 : length(levels(k$fg))
-  ggplot(k, aes(x=x, y=y, alpha=fg, color=as.integer(fg))) + geom_point(...) +
-    scale_x_continuous(breaks=xtrans(xbreaks), labels=xbreaks,
-                       minor_breaks=if(length(xminor)) xtrans(xminor)) +
-    scale_y_continuous(breaks=ytrans(ybreaks), labels=ybreaks,
-                       minor_breaks=if(length(yminor)) ytrans(yminor)) +
-    scale_color_gradientn(colors=fcolors, breaks=1 : length(levels(k$fg)),
-                          labels=levels(k$fg)) +
-    guides(alpha = FALSE, 
-           color = guide_legend(title='Frequency')) +
-    xlab(xlab) + ylab(ylab)
+
+  brn <- sort(unique(k$Freq))
+  if(length(brn) > 15) brn <- unique(quantile(k$Freq, seq(0, g) / g))
+  w <- if(nsize)
+         ggplot(k, aes(x=x, y=y, size=Freq ^ 0.25)) + geom_point(...) +
+           scale_size_continuous(breaks=brn ^ 0.25, labels=round(brn)) +
+           xlab(xlab) + ylab(ylab) +
+           guides(size = guide_legend(title='Frequency'))
+       else
+         ggplot(k, aes(x=x, y=y, alpha=fg, color=as.integer(fg))) +
+           geom_point(...) +
+           scale_color_gradientn(colors=fcolors, breaks=1 : length(levels(k$fg)),
+                                 labels=levels(k$fg)) +
+           guides(alpha = FALSE, 
+                  color = guide_legend(title='Frequency')) +
+           xlab(xlab) + ylab(ylab)
+
+  if(nx) w <- w + scale_x_continuous(breaks=xtrans(xbreaks), labels=xbreaks,
+                                     minor_breaks=if(length(xminor)) xtrans(xminor))
+  if(ny) w <- w + scale_y_continuous(breaks=ytrans(ybreaks), labels=ybreaks,
+                                     minor_breaks=if(length(yminor)) ytrans(yminor))
+  w
 }
 
 utils::globalVariables('fg')

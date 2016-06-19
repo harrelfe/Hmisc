7e692f4f29dc5a68b2ad2cffd8e6b16d61260d7d
Frank Harrell <f.harrell@vanderbilt.edu>
2016-06-19 08:37:37 -0500

    ggfreqScatter: added arguments

--- R/ggfreqScatter.r
+++ R/ggfreqScatter.r
@@ -1,11 +1,22 @@
 ggfreqScatter <- function(x, y, bins=50, g=10,
-                         xlab=as.character(substitute(x)),
-                         ylab=as.character(substitute(y)), ...) {
-  xlab <- xlab; ylab <- ylab
+                          xtrans  = function(x) x,
+                          ytrans  = function(y) y,
+                          xbreaks = pretty(x, 10),
+                          ybreaks = pretty(y, 10),
+                          xminor  = NULL,
+                          yminor  = NULL,
+                          xlab=as.character(substitute(x)),
+                          ylab=as.character(substitute(y)),
+                          fcolors=rainbow(10), ...) {
+
+  xlab <- if(! missing(xlab)) xlab else if(label(x) != '') label(x, plot=TRUE) else xlab
+  ylab <- if(! missing(ylab)) ylab else if(label(y) != '') label(y, plot=TRUE) else ylab
+
+  xbreaks <- xbreaks; ybreaks <- ybreaks
   bins <- rep(bins, length=2)
   
   i <-  ! (is.na(x) | is.na(y))
-  x <- x[i]; y <- y[i]
+  x <- xtrans(x[i]); y <- ytrans(y[i])
   
   if(is.numeric(x)) {
     rx <- range(x)
@@ -23,9 +34,13 @@ ggfreqScatter <- function(x, y, bins=50, g=10,
   if(is.numeric(y)) k$y <- as.numeric(as.character(k$y))
   k$fg <- cut2(k$Freq, g=g)
   br   <- 1 : length(levels(k$fg))
-  labs <- levels(k$fg)
   ggplot(k, aes(x=x, y=y, alpha=fg, color=as.integer(fg))) + geom_point(...) +
-    scale_color_gradientn(colors=rainbow(10), breaks=br, labels=labs) +
+    scale_x_continuous(breaks=xtrans(xbreaks), labels=xbreaks,
+                       minor_breaks=if(length(xminor)) xtrans(xminor)) +
+    scale_y_continuous(breaks=ytrans(ybreaks), labels=ybreaks,
+                       minor_breaks=if(length(yminor)) ytrans(yminor)) +
+    scale_color_gradientn(colors=fcolors, breaks=1 : length(levels(k$fg)),
+                          labels=levels(k$fg)) +
     guides(alpha = FALSE, 
            color = guide_legend(title='Frequency')) +
     xlab(xlab) + ylab(ylab)

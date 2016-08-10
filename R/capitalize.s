4f642c366f50f89546b2a263021827e68dfe7389
Derek Chiu <dchiu@bccrc.ca>
2016-08-10 12:35:49 -0700

    did not work when letters after the first had capitals

--- R/capitalize.s
+++ R/capitalize.s
@@ -1,7 +1,5 @@
 capitalize <- function(string) {
-  capped <- grep('^[^A-Z]*$', string, perl=TRUE)
-
-  substr(string[capped], 1,1) <- toupper(substr(string[capped], 1,1))
+  capped <- grep("^[A-Z]", string, invert = TRUE)
+  substr(string[capped], 1, 1) <- toupper(substr(string[capped], 1, 1))
   return(string)
 }
-

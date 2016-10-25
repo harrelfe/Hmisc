6e9bc73598fa371de5c470015dc057da1a99829a
harrelfe <f.harrell@vanderbilt.edu>
2016-10-25 08:50:18 -0500

    Ecdf: new test

--- inst/tests/Ecdf.r
+++ inst/tests/Ecdf.r
@@ -10,3 +10,11 @@ Ecdf(log(x), weights=w, lty=1:3, col=1:3, group=g, label.curves=list(keys=1:3),
 
 Ecdf(x, weights=w, lty=1:3, col=1:3, group=g, label.curves=list(keys=1:3),
      subtitles=FALSE, log='x')
+
+## From Piotr Balwierz
+reds <- rnorm(n=100, mean=5, sd=1)
+blues <- rnorm(n=100, mean=0, sd=1)
+g <- c(rep("red", length(reds)),
+       rep("blue", length(blues)))
+Ecdf(x=c(reds, blues), group=g, col=c("red", "blue"))
+Ecdf(c(reds, blues), group=factor(g), col=c('blue', 'red'))

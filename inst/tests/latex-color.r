792ecf25609624cc3f88ecae74d4f1f6317f9318
Frank Harrell <f.harrell@vanderbilt.edu>
2016-12-15 16:38:31 -0600

    tests/latex-color: improved

--- inst/tests/latex-color.r
+++ inst/tests/latex-color.r
@@ -12,10 +12,13 @@ n.row <- 9
 cell.format <- matrix(rep("", n.col*n.row), nrow=n.row,  ncol = n.col)
 #color the rows 1,4,7 blue
 cell.format[c(1,4,7),] <- "color{blue}"
+my.table[c(1,4,7),] <- 'blue'
 # color the column ‘c6’ blue
 cell.format[,6] <- "color{blue}"
+my.table[,6] <- 'blue'
 #color the cells (2,9), (3,9), (5,9), (6,9), (8,9) and (9,9) red
 cell.format[c(2,3,5,6,8,9),9] <- "color{red}"
+my.table[c(2,3,5,6,8,9),9] <- 'red'
  
  
 w <- latex(my.table,

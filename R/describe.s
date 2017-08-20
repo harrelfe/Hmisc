821604bc9c0804b56fce011eb5220c8c02a7e119
harrelfe <f.harrell@vanderbilt.edu>
2017-08-20 18:55:36 -0500

    describe: fixed bug in html.describe when there is only one variable

--- R/describe.s
+++ R/describe.s
@@ -739,42 +739,47 @@ html.describe <-
 
   R <- c(m$unicode, m$style())   ## define thinhr (and others not needed here)
   
-  if(length(at$dimensions))
+  if(length(at$dimensions)) {
     R <- c(R,
            mnb(center(bold(paste(htmlTranslate(at$descript), sskip,
-                           at$dimensions[2], ' Variables', lspace,
-                           at$dimensions[1],' Observations')))))
-
-  if(length(at$naprint)) R <- c(R, '', at$naprint)
-
-  R <- c(R, hrule)
-
-  vnames <- at$names
-  i <- 0
-  for(z in object) {
-    i <- i + 1
-    if(length(z)==0)
-      next
-
-    r <- html.describe.single(z, vname=vnames[i],
-                              tabular=tabular, greek=greek, size=size, ...)
-    R <- c(R, r, hrule)
-  }
+                                 at$dimensions[2], ' Variables', lspace,
+                                 at$dimensions[1],' Observations')))))
     
-  if(length(mv <- at$missing.vars)) {
-    R <- c(R, sskip, 'Variables with all observations missing:',
-           br, sskip)
-    mv <- paste(code(htmlTranslate(mv)), collapse=', ')
-    R <- c(R, mv)
-  }
+    if(length(at$naprint)) R <- c(R, '', at$naprint)
+    
+    R <- c(R, hrule)
+    
+    vnames <- at$names
+    i <- 0
+    for(z in object) {
+      i <- i + 1
+      if(! length(z))
+        next
+      
+      r <- html.describe.single(z, ## vname=vnames[i],
+                                tabular=tabular, greek=greek, size=size, ...)
+      R <- c(R, r, hrule)
+    }
+    
+    if(length(mv <- at$missing.vars)) {
+      R <- c(R, sskip, 'Variables with all observations missing:',
+             br, sskip)
+      mv <- paste(code(htmlTranslate(mv)), collapse=', ')
+      R <- c(R, mv)
+    }
 
-  if(scroll) R <- m$scroll(R, size=size, rows=rows, cols=cols,
-                           name=at$descript)
+    if(scroll) R <- m$scroll(R, size=size, rows=rows, cols=cols,
+                             name=at$descript)
+  }
+  else
+    R <- c(R, html.describe.single(object, tabular=tabular,
+                                   greek=greek, size=size, ...))
+  
   htmltools::HTML(R)
 }
 
 html.describe.single <-
-  function(object, vname, size=85,
+  function(object, size=85,
            tabular=TRUE, greek=TRUE, ...)
 {
   m <- markupSpecs$html

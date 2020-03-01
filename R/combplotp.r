aa45178f385e65378ef0c09b423f945e608b293c
Frank Harrell <fh@fharrell.com>
2020-03-01 11:57:06 -0600

    combplotp: fine tune width

--- R/combplotp.r
+++ R/combplotp.r
@@ -5,12 +5,15 @@
 #' Similar to the \code{UpSetR} package, draws a special dot chart sometimes called an attribute plot that depicts all possible combination of the binary variables.  By default a positive value, indicating that a certain condition pertains for a subject, is any of logical \code{TRUE}, numberic 1, \code{"yes"}, \code{"y"}, \code{"positive"}, \code{"+"} or \code{"present"} value, and all others are considered negative.  The user can override this determination by specifying her own \code{pos} function.  Case is ignored in the variable values.
 #' 
 #' The plot uses solid dots arranged in a vertical line to indicate which combination of conditions is being considered.  Frequencies of all possible combinations are shown above the dot chart.  Marginal frequencies of positive values for the input variables are shown to the left of the dot chart.  More information for all three of these component symbols is provided in hover text.
+#'
+#' Variables are sorted in descending order of marginal frqeuencies and likewise for combinations of variables.
 #' 
 #' @param formula a formula containing all the variables to be cross-tabulated, on the formula's right hand side.  There is no left hand side variable.  If \code{formula} is omitted, then all variables from \code{data} are analyzed.
 #' @param data input data frame.  If none is specified the data are assumed to come from the parent frame.
 #' @param subset an optional subsetting expression applied to \code{data}
 #' @param na.action see \code{lm} etc.
 #' @param vnames set to \code{"names"} to use variable names to label axes instead of variable labels.  When using the default \code{labels}, any variable not having a label will have its name used instead.
+#' @param includenone set to \code{TRUE} to include the combination where all conditions are absent
 #' @param showno set to \code{TRUE} to show a light dot for conditions that are not part of the currently tabulated combination
 #' @param pos a function of vector returning a logical vector with \code{TRUE} values indicating positive
 #' @param width width of \code{plotly} plot
@@ -20,17 +23,29 @@
 #' @return \code{plotly} object
 #' @author Frank Harrell
 #' @examples
-#' g <- function() sample(0:1, 100, replace=TRUE)
-#' set.seed(2)
+#' g <- function() sample(0:1, n, prob=c(1 - p, p), replace=TRUE)
+#' set.seed(2); n <- 100; p <- 0.5
 #' x1 <- g(); label(x1) <- 'A long label for x1 that describes it'
 #' x2 <- g()
 #' x3 <- g(); label(x3) <- 'This is<br>a label for x3'
 #' x4 <- g()
-#' combplotp(~ x1 + x2 + x3 + x4, showno=TRUE)
+#' combplotp(~ x1 + x2 + x3 + x4, showno=TRUE, includenone=TRUE)
+#'
+#' n <- 1500; p <- 0.05
+#' pain       <- g()
+#' anxiety    <- g()
+#' depression <- g()
+#' soreness   <- g()
+#' numbness   <- g()
+#' tiredness  <- g()
+#' sleepiness <- g()
+#' combplotp(~ pain + anxiety + depression + soreness + numbness +
+#'           tiredness + sleepiness, showno=TRUE)
 #' @export
 
 combplotp <- function(formula, data=NULL, subset, na.action=na.retain,
-                      vnames=c('labels', 'names'), showno=FALSE,
+                      vnames=c('labels', 'names'),
+                      includenone=FALSE, showno=FALSE,
                       pos=function(x) 1 * (toupper(x) %in% 
                         c('true', 'yes', 'y', 'positive', '+', 'present', '1')),
                       width=NULL, height=NULL,
@@ -61,8 +76,18 @@ combplotp <- function(formula, data=NULL, subset, na.action=na.retain,
   # Sort variables in order of descending marginal frequency
   Y <- Y[order(m)]
   f <- as.data.frame(table(Y, ...))
+  f <- f[f$Freq > 0, ]   # subset didn't work
+  p <- ncol(f) - 1       # no. variables
+  if(! includenone) {
+    numcondpresent <- apply(f[, 1 : p], 1, function(u) sum(u == 1))
+    if(any(numcondpresent == 0))
+      f <- f[numcondpresent > 0, ]
+  }
+  # Sort combinations in descending order of frequency
+  i <- order(-f$Freq)
+  f <- f[i, ]
+  
   n <- nrow(f)        # no. combinations
-  p <- ncol(f) - 1    # no. variables
   X <- as.matrix(1 * (f[, 1 : p] == '1'))
   Freq <- f$Freq
   
@@ -82,12 +107,13 @@ combplotp <- function(formula, data=NULL, subset, na.action=na.retain,
   }
   
   hdc <- plotlyParm$heightDotchartb
-  if(! length(height)) height <- hdc(c(labs, '', ''), low=250)
+  if(! length(height)) height <- hdc(c(labs, '', ''), low=250, per=30)
   if(! length(width)) {
     # Find longest html line in labs
     w <- unlist(strsplit(labs, '<br>'))
-    longest <- w[which.max(nchar(w))]
-    width <- hdc(c(rep('', n), longest))
+    longest  <- w[which.max(nchar(w))]
+    nlongest <- nchar(longest)
+    width <- hdc(rep('X', n), per=23, low=450) + 8 * nlongest
   }
   P <- plotly::plot_ly(height=height, width=width)
   
@@ -97,13 +123,13 @@ combplotp <- function(formula, data=NULL, subset, na.action=na.retain,
                             x = ~ rep(-2, p), xend = ~ rep(n, p),
                             y = ~ 1 : p, yend = ~ 1 : p,
                             color = I('gray80'), line=list(width=0.75),
-                            showlegend=FALSE)
+                            hoverinfo='none', showlegend=FALSE)
   
   P <- plotly::add_segments(P,
                             x = ~ 1 : n, xend = ~ 1 : n,
                             y = ~ rep(1, n), yend = ~ rep(p + 1.5, n),
                             color = I('gray80'), line=list(width=0.75),
-                            showlegend=FALSE)
+                            hoverinfo='none', showlegend=FALSE)
 
   # Show main result as dot chart
   P <- plotly::add_markers(P,
@@ -163,16 +189,17 @@ combplotp <- function(formula, data=NULL, subset, na.action=na.retain,
   
   # Add variable labels as annotations
   P <- plotly::add_text(P,
-                        x = ~ rep(n + 1, p), y = 1 : p,
+                        x = ~ rep(n + 0.7, p), y = 1 : p,
                         text = ~ labs[namx],
                         textposition="middle right",
+                        hoverinfo='none',
                         showlegend=FALSE)
   
                           
-  nc <- nchar(longest)  # leave extra space for long label
+  # leave extra space for long label
   P <- plotly::layout(P,
                       xaxis = list(title='', tickvals=1 : n,
-                                   range=c(-2, n + 0.4 * nc),
+                                   range=c(-2, n + 0.4 * nlongest),
                                    showgrid=FALSE,
                                    showticklabels=FALSE, zeroline=FALSE),
                       yaxis = list(title='', tickvals=1 : p,

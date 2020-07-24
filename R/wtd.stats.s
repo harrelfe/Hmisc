3fc06b2034647cb403e472268cabea66263643c3
Bert <b02902019@ntu.edu.tw>
2020-07-25 01:26:59 +0800

    Update wtd.stats.s
    Update naming

--- R/wtd.stats.s
+++ R/wtd.stats.s
@@ -69,17 +69,17 @@ wtd.quantile <- function(x, weights=NULL, probs=c(0, .25, .5, .75, 1),
     w <- wtd.table(x, weights, na.rm=na.rm, normwt=normwt, type='list')
     x     <- w$x
     wts   <- w$sum.of.weights
-    weighted_S = c()
+    weighted_s = c()
     cum_w <- cumsum(wts)
     for (i in c(1:length(wts))){
       if (i > 1){
-        Sk = (i-1) * wts[i] + (length(wts)-1) * cum_w[i-1]}
+        sk = (i-1) * wts[i] + (length(wts)-1) * cum_w[i-1]}
       else{
-        Sk = 0
+        sk = 0
       }
-      weighted_S = append(weighted_S, Sk)
+      weighted_s = append(weighted_s, sk)
     }
-    allq <- approx(weighted_S, x, xout=probs*weighted_S[length(weighted_S)], 
+    allq <- approx(weighted_s, x, xout=probs*weighted_s[length(weighted_s)], 
                    method='linear', f=1, rule=2)$y
     names(quantiles) <- nams
     return(quantiles)

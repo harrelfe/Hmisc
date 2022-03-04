53aa4c9c27ebd8bcf3463823e1d23eedff34abac
harrelfe <fh@fharrell.com>
2022-03-03 21:13:43 -0600

    simPOcuts: used names(p) for y levels

--- R/popower.s
+++ R/popower.s
@@ -103,18 +103,18 @@ simPOcuts <- function(n, nsim=10, odds.ratio=1, p) {
     stop('probabilities in p must sum to 1')
   p0   <- p
   p1   <- pomodm(p=p0, odds.ratio=odds.ratio)
-  yval <- 0 : (length(p) - 1)
-  ycuts <- yval[-1]
-  or <- matrix(NA, nrow=nsim, ncol=max(yval),
+  lp   <- length(p)
+  yval <- if(length(names(p))) names(p) else as.character(1 : lp)
+  or <- matrix(NA, nrow=nsim, ncol=lp - 1,
                dimnames=list(paste('Simulation', 1 : nsim),
-                             paste0('y>=', ycuts)))
+                             paste0('y>=', yval[-1])))
   for(i in 1 : nsim) {
-    y0 <- sample(yval, n / 2, prob=p0,  replace=TRUE)
-    y1 <- sample(yval, n / 2, prob=p1,  replace=TRUE)
-    for(ycut in ycuts){
+    y0 <- sample(1 : lp, n / 2, prob=p0,  replace=TRUE)
+    y1 <- sample(1 : lp, n / 2, prob=p1,  replace=TRUE)
+    for(ycut in 2 : lp){
       prop0 <- mean(y0 >= ycut)
       prop1 <- mean(y1 >= ycut)
-      or[i, ycut] <- (prop1 / (1. - prop1)) / (prop0 / (1. - prop0))
+      or[i, ycut - 1] <- (prop1 / (1. - prop1)) / (prop0 / (1. - prop0))
     }
   }
   or

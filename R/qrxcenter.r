f3cbbdbe5c4206a2cd1ac921bd46e1622f8e0d72
harrelfe <fh@fharrell.com>
2024-11-18 19:15:40 -0600

    re-wrote rcorr Fortran subroutine using vectorization and improved numerical algorithm    * added function qrxcenter

---
+++ R/qrxcenter.r
@@ -0,0 +1,46 @@
+#' Mean-center a data matrix and QR transform it
+#'
+#' For a numeric matrix `x` (or a numeric vector that is automatically changed to a one-column matrix), computes column means and subtracts them from `x` columns, and passes this matrix to [base::qr()] to orthogonalize columns.  Columns of the transformed `x` are negated as needed so that original directions are preserved (which are arbitrary with QR decomposition).  Instead of the default `qr` operation for which sums of squares of column values are 1.0, `qrxcenter` makes all the transformed columns have standard deviation of 1.0.
+#' @title qrxcenter
+#' @param x a numeric matrix or vector with at least 2 rows
+#'
+#' @return a list with components `x` (transformed data matrix), `R` (the matrix that can be used to transform raw `x` and to transform regression coefficients computed on transformed `x` back to the original space), `Ri` (transforms transformed `x` back to original scale except for `xbar`), and `xbar` (vector of means of original `x` columns`)
+#' @export
+#' @md
+#'
+#' @examples
+#' set.seed(1)
+#' age <- 1:10
+#' country <- sample(c('Slovenia', 'Italy', 'France'), 10, TRUE)
+#' x <- model.matrix(~ age + country)[, -1]
+#' x
+#' w <- qrxcenter(x)
+#' w
+#' # Reproduce w$x
+#' sweep(x, 2, w$xbar) %*% w$R
+#' # Reproduce x from w$x
+#' sweep(w$x %*% w$Ri, 2, w$xbar, FUN='+')
+qrxcenter <- function(x) {
+  if(! is.matrix(x)) x <- is.matrix(x)
+  d    <- dim(x)
+  n    <- d[1]
+  p    <- d[2]
+  if(n <= 2) stop('requires n > 1')
+
+  x    <- scale(x, center=TRUE, scale=FALSE)
+  xbar <- as.vector(attr(x, 'scaled:center'))  
+  QR   <- qr(x)
+  Q    <- qr.Q(QR)
+  RR   <- qr.R(QR)
+  sgns <- sign(diag(RR))
+  # Each column of Q squared sums to 1; make sum of squares = n - 1
+  # This makes SDs 1.0
+  sn <- sqrt(n - 1)
+  x     <- sweep(Q,  MARGIN = 2, STATS = sgns, FUN = `*`) * sn
+  RR    <- sweep(RR, MARGIN = 1, STATS = sgns, FUN = `*`) / sn
+
+  list(x=x, R = backsolve(RR, diag(p)), Ri = RR, xbar=xbar)
+}
+
+
+
--- src/rcorr.f90
+++ src/rcorr.f90
@@ -10,114 +10,57 @@ subroutine rcorr(xx, n, p, itype, dmat, npair, x, y, rx, ry, work, iwork)
     integer, intent(out) :: npair(p, p)
     real(REAL64), intent(out) :: x(n), y(n), rx(n), ry(n), work(n)
     integer, intent(out) :: iwork(n)
-    real(REAL64) :: sumx, sumx2, sumy, sumy2, sumxy, z, a, b, xk, yk, d
-    integer :: i, j, k, m, np
+    real(REAL64) :: meanx, meany, vx, vy, xyn
+    integer :: i, j, m
+    logical, allocatable :: nna(:)
+    real(REAL64), parameter :: nab = 1.0e49_real64   ! signifies missing value
   
-    ! Initialize sums to prevent warnings
-    sumx = 0.0_real64
-    sumy = 0.0_real64
-    sumx2 = 0.0_real64
-    sumy2 = 0.0_real64
-    sumxy = 0.0_real64
+    allocate(nna(n))
   
+    dmat = 1.0e50_real64
+
     ! Loop through each pair of variables
     do i = 1, p
-      np = 0
-      do k = 1, n
-        if (xx(k, i) < 1.0e49_real64) np = np + 1
-      end do
-      npair(i, i) = np
+      dmat(i, i)  = 1e0_real64
+      npair(i, i) = count(xx(:, i) < nab)
   
       do j = i + 1, p
-        m = 0
-        if (itype == 1) then
-          sumx = 0.0_real64
-          sumy = 0.0_real64
-          sumx2 = 0.0_real64
-          sumy2 = 0.0_real64
-          sumxy = 0.0_real64
-        end if
-  
-        do k = 1, n
-          xk = xx(k, i)
-          yk = xx(k, j)
-          if (xk < 1.0e49_real64 .and. yk < 1.0e49_real64) then
-            m = m + 1
-            if (itype == 1) then
-              a = xk
-              b = yk
-              sumx = sumx + a
-              sumx2 = sumx2 + a * a
-              sumy = sumy + b
-              sumy2 = sumy2 + b * b
-              sumxy = sumxy + a * b
-            else
-              x(m) = xk
-              y(m) = yk
-            end if
-          end if
-        end do
-  
+        nna = (xx(:, i) < nab) .and. (xx(:, j) < nab)
+        m   = count(nna)
         npair(i, j) = m
-        if (m > 1) then
-          if (itype == 1) then
-            z = real(m, REAL64)
-            d = (sumxy - sumx * sumy / z) / sqrt((sumx2 - sumx * sumx / z) * (sumy2 - sumy * sumy / z))
-          else
-            call docorr(x, y, m, d, rx, ry, work, iwork)
-          end if
-          dmat(i, j) = d
-        else
-          dmat(i, j) = 1.0e50_real64
+        if(m < 2) cycle
+
+        x(1:m) = pack(xx(:, i), nna)
+        y(1:m) = pack(xx(:, j), nna)
+
+        if(itype == 2) then     ! replace x and y with their ranks
+          call rank(m, x, work, iwork, rx)
+          call rank(m, y, work, iwork, ry)
+          x(1:m) = rx(1:m)
+          y(1:m) = ry(1:m)
         end if
+
+        xyn = real(m, REAL64)
+        meanx = sum(x(1:m)) / xyn
+        meany = sum(y(1:m)) / xyn
+        x(1:m) = x(1:m) - meanx
+        y(1:m) = y(1:m) - meany
+        vx    = sum(x(1:m) ** 2)
+        vy    = sum(y(1:m) ** 2)
+        if(vx .eq. 0e0_real64 .or. vy .eq. 0e0_real64) cycle
+        dmat(i, j) = sum(x(1:m) * y(1:m)) / sqrt(vx * vy)
       end do
     end do
   
-    ! Set the diagonal and symmetry
+    ! Finish symmetric matrices
     do i = 1, p
-      dmat(i, i) = 1.0_real64
       do j = i + 1, p
-        dmat(j, i) = dmat(i, j)
+        dmat(j, i)  = dmat(i, j)
         npair(j, i) = npair(i, j)
       end do
     end do
   
+    deallocate(nna)
     return
   end subroutine rcorr
-  
-  subroutine docorr(x, y, n, d, rx, ry, work, iwork)
-    use ISO_FORTRAN_ENV
-    implicit none
-    integer, intent(in) :: n
-    real(REAL64), intent(in) :: x(n), y(n)
-    real(REAL64), intent(out) :: d
-    real(REAL64), intent(out) :: rx(n), ry(n), work(n)
-    integer, intent(out) :: iwork(n)
-    real(REAL64) :: sumx, sumx2, sumy, sumy2, sumxy, a, b, z
-    integer :: i
-  
-    call rank(n, x, work, iwork, rx)
-    call rank(n, y, work, iwork, ry)
-  
-    sumx = 0.0_real64
-    sumx2 = 0.0_real64
-    sumy = 0.0_real64
-    sumy2 = 0.0_real64
-    sumxy = 0.0_real64
-  
-    do i = 1, n
-      a = rx(i)
-      b = ry(i)
-      sumx = sumx + a
-      sumx2 = sumx2 + a * a
-      sumy = sumy + b
-      sumy2 = sumy2 + b * b
-      sumxy = sumxy + a * b
-    end do
-  
-    z = real(n, REAL64)
-    d = (sumxy - sumx * sumy / z) / sqrt((sumx2 - sumx * sumx / z) * (sumy2 - sumy * sumy / z))
-  
-    return
-  end subroutine docorr
   

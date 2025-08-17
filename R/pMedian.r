8a1981970552fcae849619e1f86500be21b824da
harrelfe <fh@fharrell.com>
2025-08-17 10:30:25 -0400

    pMedian: added bootstrap confidence limits; added boot to SUGGESTED packages

--- R/pMedian.r
+++ R/pMedian.r
@@ -1,26 +1,33 @@
 #' Pseudomedian
 #' 
-#' Uses fast Fortran code to compute the pseudomedian of a numeric vector.  The pseudomedian is the median of all possible midpoints of two observations.  The pseudomedian is also called the Hodges-Lehmann one-sample estimator.  The Fortran code is was originally from JF Monahan, and was converted to C++ in the `DescTools` package.  It has been converted to Fortran 2018 here.
+#' Uses fast Fortran code to compute the pseudomedian of a numeric vector.  The pseudomedian is the median of all possible midpoints of two observations.  The pseudomedian is also called the Hodges-Lehmann one-sample estimator.  The Fortran code is was originally from JF Monahan, and was converted to C++ in the `DescTools` package.  It has been converted to Fortran 2018 here.  Bootstrap confidence intervals are optionally computed.
 #' 
-#' If n > 250,000 a random sample of 250,000 values of `x` is used to limit execution time.
+#' If n > 250,000 a random sample of 250,000 values of `x` is used to limit execution time.  For n > 1,000 only the percentile bootstrap confidence interval is computed.
+#' 
+#' Bootstrapping uses the Fortran subroutine directly, for efficiency.
 #' 
 #' @title pMedian
 #' @param x a numeric vector
 #' @param na.rm set to `TRUE` to exclude `NA`s before computing the pseudomedian
-#'
-#' @return a scalar numeric value
+#' @param conf.int confidence level, defaulting to 0 so that no confidence limits are computed.  Set to a number between 0 and 1 to compute bootstrap confidence limits
+#' @param B number of bootstrap samples if `conf.int > 0`
+#' @param type type of bootstrap interval, defaulting to `'percentile'`; may also be `'bca'`
+#
+#' @return a scalar numeric value if `conf.int = 0`, or a 3-vector otherwise, with named elements `estimate, lower, upper`.  If the number of non-missing values is less than 5, `NA` is returned for both lower and upper limits.
 #' @export
 #' @md
 #' @seealso <https://dl.acm.org/toc/toms/1984/10/3/>, <https://www4.stat.ncsu.edu/~monahan/jul10/>
 #' @examples
 #' x <- c(1:4, 10000)
 #' pMedian(x)
+#' pMedian(x, conf.int=0.95)
 #' # Compare with brute force calculation and with wilcox.test
 #' w <- outer(x, x, '+')
 #' median(w[lower.tri(w, diag=TRUE)]) / 2
 #' wilcox.test(x, conf.int=TRUE)
 
-pMedian <- function(x, na.rm = FALSE) {
+pMedian <- function(x, na.rm = FALSE, conf.int=0, B=1000, type=c('percentile', 'bca')) {
+  type <- match.arg(type)
   if(na.rm) x <- x[! is.na(x)]
   n <- length(x)
   if(n == 0) return(NA_real_)
@@ -32,6 +39,24 @@ pMedian <- function(x, na.rm = FALSE) {
     x <- sample(x, nmax)
     warning('n > 250000; pMedian is using a random sample of 250000 values')
   }
-  .Fortran(F_hlqest, as.double(sort(x)), as.double(runif(1000)), as.integer(n), result=double(1))$result
-
+  w   <- as.double(runif(1000))
+  n   <- as.integer(n)
+  res <- .Fortran(F_hlqest, as.double(sort(x)), w, n,
+                  result=double(1))$result
+  if(conf.int == 0) return(res)
+  if(n < 5) return(c(estimate=res, lower=NA, upper=NA))
+  if(! requireNamespace('boot', quietly=TRUE))
+    stop('conf.int > 0 requires the boot package to be installed')
+  comp <- function(x, i)
+    .Fortran(F_hlqest, as.double(sort(x[i])), w, n,
+                  result=double(1))$result
+  b  <- boot::boot(x, statistic=comp, R=B)
+  if(type == 'bca' && n > 1000) {
+    warning('type="bca" is too slow for n > 1000; reverting to percentile method')
+    type <- 'percentile'
+  }
+  cl <- boot::boot.ci(b, type=if(type == 'percentile') 'perc' else 'bca',
+                      conf=conf.int)[[if(type == 'percentile') 'percent' else 'bca']]
+  l <- length(cl)
+  c(estimate=res, lower=cl[l - 1], upper=cl[l])
 }

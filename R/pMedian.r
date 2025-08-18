3d38051edfc7e677c7a2a732e49382eafcca84b6
harrelfe <fh@fharrell.com>
2025-08-18 08:44:43 -0400

    pMedian: refined default bootstrap type

--- R/pMedian.r
+++ R/pMedian.r
@@ -11,12 +11,12 @@
 #' @param na.rm set to `TRUE` to exclude `NA`s before computing the pseudomedian
 #' @param conf.int confidence level, defaulting to 0 so that no confidence limits are computed.  Set to a number between 0 and 1 to compute bootstrap confidence limits
 #' @param B number of bootstrap samples if `conf.int > 0`
-#' @param type type of bootstrap interval, defaulting to `'percentile'`; may also be `'bca'`
+#' @param type type of bootstrap interval, defaulting to `'percentile'` for n >= 150 or `'bca'` for n < 150
 #
-#' @return a scalar numeric value if `conf.int = 0`, or a 3-vector otherwise, with named elements `estimate, lower, upper`.  If the number of non-missing values is less than 5, `NA` is returned for both lower and upper limits.
+#' @return a scalar numeric value if `conf.int = 0`, or a 3-vector otherwise, with named elements `estimate, lower, upper` and attribute `type`.  If the number of non-missing values is less than 5, `NA` is returned for both lower and upper limits.
 #' @export
 #' @md
-#' @seealso <https://dl.acm.org/toc/toms/1984/10/3/>, <https://www4.stat.ncsu.edu/~monahan/jul10/>
+#' @seealso <https://dl.acm.org/toc/toms/1984/10/3/>, <https://www4.stat.ncsu.edu/~monahan/jul10/>, <https://fharrell.com/post/aci/>
 #' @examples
 #' x <- c(1:4, 10000)
 #' pMedian(x)
@@ -27,12 +27,14 @@
 #' wilcox.test(x, conf.int=TRUE)
 
 pMedian <- function(x, na.rm = FALSE, conf.int=0, B=1000, type=c('percentile', 'bca')) {
-  type <- match.arg(type)
+  mtype <- missing(type)
+  type  <- match.arg(type)
   if(na.rm) x <- x[! is.na(x)]
   n <- length(x)
-  if(n == 0) return(NA_real_)
-  if(n == 1) return(as.double(x))
-  if(! na.rm && anyNA(x)) return(NA_real_)
+  g <- function(z) if(conf.int == 0) z else c(estimate=z, lower=NA_real_, upper=NA_real_)
+  if(n == 0) return(g(NA_real_))
+  if(n == 1) return(g(as.double(x)))
+  if(! na.rm && anyNA(x)) return(g(NA_real_))
   nmax <- 250000L
   if(n > nmax) {
     n <- nmax
@@ -51,6 +53,7 @@ pMedian <- function(x, na.rm = FALSE, conf.int=0, B=1000, type=c('percentile', '
     .Fortran(F_hlqest, as.double(sort(x[i])), w, n,
                   result=double(1))$result
   b  <- boot::boot(x, statistic=comp, R=B)
+  if(mtype) type <- if(n >= 150) 'percentile' else 'bca'
   if(type == 'bca' && n > 1000) {
     warning('type="bca" is too slow for n > 1000; reverting to percentile method')
     type <- 'percentile'
@@ -58,5 +61,7 @@ pMedian <- function(x, na.rm = FALSE, conf.int=0, B=1000, type=c('percentile', '
   cl <- boot::boot.ci(b, type=if(type == 'percentile') 'perc' else 'bca',
                       conf=conf.int)[[if(type == 'percentile') 'percent' else 'bca']]
   l <- length(cl)
-  c(estimate=res, lower=cl[l - 1], upper=cl[l])
+  res <- c(estimate=res, lower=cl[l - 1], upper=cl[l])
+  attr(res, 'type') <- type
+  res
 }

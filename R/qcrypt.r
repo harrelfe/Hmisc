aa9c27d3ca29df45e265cac17c3d52a2f4d8e18f
harrelfe <fh@fharrell.com>
2026-08-13 08:44:53 -0500

    "Changed qcrypt to use qs2 package instead of qs"

--- R/qcrypt.r
+++ R/qcrypt.r
@@ -3,10 +3,10 @@
 ##' `qcrypt` is used to protect sensitive information on a user's computer or when transmitting a copy of the file to another R user.  Unencrypted information only exists for a moment, and the encryption password does not appear in the user's script but instead is managed by the `keyring` package to remember the password across R sessions, and the `getPass` package, which pops up a password entry window and does not allow the password to be visible.  The password is requested only once, except perhaps when the user logs out of their operating system session or reboots.
 ##'
 ##' The keyring can be bypassed and the password entered in a popup window by specifying `service=NA`.  This is the preferred approach when sending an encrypted file to a user on a different computer.
-##' 
-##' `qcrypt` writes R objects to disk in a temporary file using the `qs` package `qsave` function.  The file is quickly encrypted using the `safer` package, and the temporary unencrypted `qs` file is deleted.  When reading an encrypted file the process is reversed.
 ##'
-##' To save an object in an encrypted file, specify the object as the first argument `obj` and specify a base file name as a character string in the second argument `base`.  The full `qs` file name will be of the form `base.qs.encrypted` in the user's current working directory.  To unencrypt the file into a short-lived temporary file and use `qs::qread` to read it, specify the base file name as a character string with the first argument, and do not specify the `base` argument.
+##' `qcrypt` writes R objects to disk in a temporary file using the `qs2` package `qs_save` function.  The file is quickly encrypted using the `safer` package, and the temporary unencrypted `qs2` file is deleted.  When reading an encrypted file the process is reversed and uses `qs2::qs_read`.
+##'
+##' To save an object in an encrypted file, specify the object as the first argument `obj` and specify a base file name as a character string in the second argument `base`.  The full `qs2` file name will be of the form `base.qs2.encrypted` in the user's current working directory.  To unencrypt the file into a short-lived temporary file and use `qs2::qs_read` to read it, specify the base file name as a character string with the first argument, and do not specify the `base` argument.
 ##'
 ##' Alternatively, `qcrypt` can be used to encrypt or decrypt existing files of any type using the same password and keyring mechanism.  The former is done by specifying `file` that does not end in `'.encrypted'` and the latter is done by ending `file` with `'.encrypted'`.  When `file` does not contain a path it is assumed to be in the current working directory.  When a file is encrypted the original file is removed.  Files are decrypted into a temporary directory created by `tempdir()`, with the name of the file being the value of `file` with `'.encrypted'` removed.
 ##'
@@ -31,9 +31,9 @@
 ##' # The first time qcrypt is run with a service a password will
 ##' # be requested.  It will be remembered across sessions thanks to
 ##' # the keyring package
-##' qcrypt(x, 'x')   # creates x.qs.encrypted in current working directory
-##' x <- qcrypt('x') # unencrypts x.qs.encrypted into a temporary
-##'                  # directory, uses qs::qread to read it, and
+##' qcrypt(x, 'x')   # creates x.qs2.encrypted in current working directory
+##' x <- qcrypt('x') # unencrypts x.qs2.encrypted into a temporary
+##'                  # directory, uses qs2::qs_read to read it, and
 ##'                  # stores the result in x
 ##' # Encrypt a general file using a different password
 ##' qcrypt(file='report.pdf', service='pdfkey')
@@ -47,63 +47,73 @@
 ##' pw <- 'somepassword'     # run this in the console
 ##' x <- qcrypt('x', pw=pw)  # interactively run this in a script
 ##' }
-qcrypt <- function(obj, base, service='R-keyring-service', file, pw) {
-  if(missing(pw)) {
-    if(! is.na(service) && ! requireNamespace('keyring', quietly=TRUE))
-      stop('you must install the keyring package to use qcrypt with service != NA')
-    if(! requireNamespace('getPass', quietly=TRUE))
-      stop('you must install the getPass package to use qcrypt')
-    if(! requireNamespace('qs', quietly=TRUE))
-      stop('you must install the qs package to use qcrypt')
-    if(! requireNamespace('safer', quietly=TRUE))
-      stop('you must install the safer package to use qcrypt')
+qcrypt <- function(obj, base, service = "R-keyring-service", file, pw) {
+  if (missing(pw)) {
+    if (!is.na(service) && !requireNamespace("keyring", quietly = TRUE)) {
+      stop("you must install the keyring package to use qcrypt with service != NA")
+    }
+    if (!requireNamespace("getPass", quietly = TRUE)) {
+      stop("you must install the getPass package to use qcrypt")
+    }
+    if (!requireNamespace("qs2", quietly = TRUE)) {
+      stop("you must install the qs2 package to use qcrypt")
+    }
+    if (!requireNamespace("safer", quietly = TRUE)) {
+      stop("you must install the safer package to use qcrypt")
+    }
 
-    if(is.na(service)) {
-      prompt <- if(! missing(base)) 'Define password for storing encrypted data: '
-      else 'Enter password previously used to store data: '
-      pw <- getPass::getPass(msg = prompt, noblank=TRUE)
+    if (is.na(service)) {
+      prompt <- if (!missing(base)) {
+        "Define password for storing encrypted data: "
       } else {
-      pw <- tryCatch(keyring::key_get(service), error = function(...) '')
+        "Enter password previously used to store data: "
+      }
+      pw <- getPass::getPass(msg = prompt, noblank = TRUE)
+    } else {
+      pw <- tryCatch(keyring::key_get(service), error = function(...) "")
 
-      if(pw == '') {
-        prompt <- if(! missing(base)) 'Define password for storing encrypted data: '
-        else 'Enter password previously used to store data: '
-        pw <- getPass::getPass(msg = prompt, noblank=TRUE)
-        keyring::key_set_with_value(service, password=pw)
+      if (pw == "") {
+        prompt <- if (!missing(base)) {
+          "Define password for storing encrypted data: "
+        } else {
+          "Enter password previously used to store data: "
+        }
+        pw <- getPass::getPass(msg = prompt, noblank = TRUE)
+        keyring::key_set_with_value(service, password = pw)
       }
       pw <- keyring::key_get(service)
     }
   }
   tf <- tempfile()
 
-  if(! missing(file)) {
-    if(! grepl('\\.encrypted$', file)) {  # encrypt
-      f <- paste0(file, '.encrypted')
-      unlink(f)   # just in case; encrypt_file throws an error if file exists
-      safer::encrypt_file(file, key=pw, outfile=f)
+  if (!missing(file)) {
+    if (!grepl("\\.encrypted$", file)) { # encrypt
+      f <- paste0(file, ".encrypted")
+      unlink(f) # just in case; encrypt_file throws an error if file exists
+      safer::encrypt_file(file, key = pw, outfile = f)
       unlink(file)
       return(invisible(f))
     } else { # decrypt
-      fout <- paste(tempdir(), sub('\\.encrypted$', '', basename(file)), sep='/')
-      safer::decrypt_file(file, key=pw, outfile=fout)
+      fout <- paste(tempdir(), sub("\\.encrypted$", "", basename(file)), sep = "/")
+      safer::decrypt_file(file, key = pw, outfile = fout)
       return(invisible(fout))
     }
   }
 
-  if(! missing(base)) {  # base specified -> create file
-    qs::qsave(obj, tf)
-    f <- paste0(base, '.qs.encrypted')
-    unlink(f)    # just in case
-    safer::encrypt_file(tf, key=pw, outfile=f)
-    unlink(tf)   # quickly remove unencrypted file
+  if (!missing(base)) { # base specified -> create file
+    qs2::qs_save(obj, tf)
+    f <- paste0(base, ".qs2.encrypted")
+    unlink(f) # just in case
+    safer::encrypt_file(tf, key = pw, outfile = f)
+    unlink(tf) # quickly remove unencrypted file
     return(invisible(f))
   }
 
   # base is missing and base file name is assumed to be in the first argument, obs
 
-  f <- paste0(obj, '.qs.encrypted')
-  safer::decrypt_file(f, key=pw, outfile=tf)
-  x <- qs::qread(tf)
-  unlink(tf)    # quickly remove unencrypted file
-  x 
+  f <- paste0(obj, ".qs2.encrypted")
+  safer::decrypt_file(f, key = pw, outfile = tf)
+  x <- qs2::qs_read(tf)
+  unlink(tf) # quickly remove unencrypted file
+  x
 }
